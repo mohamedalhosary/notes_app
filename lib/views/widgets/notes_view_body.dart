56777ca0f792f91ad3569795e1eb7279f88f7349
mohamedalhosary <malhosary328@gmail.com>
2024-03-04 17:04:41 +0200

    add floating action button

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quran_app/views/widgets/custom_app_bar.dart';
 import 'package:quran_app/views/widgets/custom_note_item.dart';
+import 'package:quran_app/views/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
