d6c6c501e15ef1e5f90bda9e7842e39ad0c8bdc9
mohamedalhosary <malhosary328@gmail.com>
2024-03-08 10:48:43 +0200

    add custom app bar to edit note view

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -3,17 +3,23 @@ import 'package:quran_app/views/widgets/custom_note_item.dart';
 import 'package:quran_app/views/widgets/custom_search_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
+  const CustomAppBar({super.key, required this.title, required this.icon});
 
+  final String title;
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: const [
-        Text('Notes' ,style: TextStyle(fontSize: 28,),),
+    return  Row(
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 28,
+          ),
+        ),
         Spacer(),
-        CustomSearchIcon()
+        CustomIcon(icon: icon,)
       ],
     );
   }
 }
-
--- lib/views/widgets/custom_search_icon.dart
+++ lib/views/widgets/custom_search_icon.dart
@@ -1,19 +1,22 @@
-
 import 'package:flutter/material.dart';
 
-class CustomSearchIcon extends StatelessWidget {
-  const CustomSearchIcon({super.key});
-
+class CustomIcon extends StatelessWidget {
+  const CustomIcon({super.key, required this.icon});
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 46,
       width: 46,
-      decoration: BoxDecoration(color: Colors.white.withOpacity(.05),
-      borderRadius: BorderRadius.circular(16),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(.05),
+        borderRadius: BorderRadius.circular(16),
       ),
-      child: const Center(
-        child: Icon(Icons.search,size: 28,),
+      child: Center(
+        child: Icon(
+          icon,
+          size: 28,
+        ),
       ),
     );
   }
