cb8a1c58596cf9a08c0bdaeb23b52cbd0529be6c
mohamedalhosary <malhosary328@gmail.com>
2024-03-09 00:38:38 +0200

    add note cubit

--- lib/cubit/add_note_cubit/add_note_cubit_cubit.dart
+++ lib/cubit/add_note_cubit/add_note_cubit_cubit.dart
@@ -1,8 +1,25 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:quran_app/models/note_models.dart';
+import 'package:quran_app/views/widgets/constants.dart';
 
 part 'add_note_cubit_state.dart';
 
 class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
   AddNoteCubitCubit() : super(AddNoteCubitInitial());
+
+  addNote(NoteModels note) async {
+    emit(AddNoteCubitLoading());
+    Hive.box(kNotesBox);
+
+    try {
+      var notesBox = Hive.box(kNotesBox);
+
+      emit(AddNoteCubitSuccess());
+      await notesBox.add(note);
+    } catch (e) {
+      AddNoteCubitFailure(e.toString());
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:quran_app/models/note_models.dart';
 import 'package:quran_app/views/notes_views.dart';
 import 'package:quran_app/views/widgets/constants.dart';
 
@@ -7,6 +8,7 @@ void main() async {
   await Hive.initFlutter();
 
   await Hive.openBox(kNotesBox);
+  Hive.registerAdapter(NoteModelsAdapter());
   runApp(const MyApp());
 }
 // this notes app by eng mohamed alhosary
