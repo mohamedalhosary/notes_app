b12a26eb555cdd9e1152e417036c6426784fa078
mohamedalhosary <malhosary328@gmail.com>
2024-03-09 11:04:05 +0200

    add simple bloc

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:quran_app/cubit/add_note_cubit/add_note_cubit_cubit.dart';
 import 'package:quran_app/models/note_models.dart';
+import 'package:quran_app/simple_bloc_observer.dart';
 import 'package:quran_app/views/notes_views.dart';
 import 'package:quran_app/views/widgets/constants.dart';
 
 void main() async {
   await Hive.initFlutter();
 
+  Bloc.observer = SimpleBlocObserver();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelsAdapter());
   runApp(const MyApp());
@@ -26,7 +28,7 @@ class MyApp extends StatelessWidget {
           create: (context) => AddNoteCubitCubit(),
         ),
       ],
-      child:  MaterialApp(
+      child: MaterialApp(
         theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Unbounded'),
         debugShowCheckedModeBanner: false,
         home: const NotesView(),
